629ba714b44ed0c619a917d1e3eae6cb86b80185
sohanemon <m.sohanemon@gmail.com>
2022-08-26 22:30:00 +0600

    modal section done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const main = document.getElementById("main");
 
 const createElement = (type, src, fpk, capacity) => {
   const div = document.createElement("div");
-  div.className = "card w-96 mx-auto shadow-xl image-full";
+  div.className = "card w-96 mx-auto shadow-xl ";
   div.innerHTML = `
         <figure>
           <img src=${src} alt="Shoes" />
@@ -16,7 +16,31 @@ const createElement = (type, src, fpk, capacity) => {
             <span>Fare Per Kilo : ${fpk}</span><span>Capacity: ${capacity}</span>
           </div>
           <div class="card-actions justify-end">
-            <button class="btn btn-secondary text-white">Lets Go</button>
+          <label for="my-modal-4" class="btn modal-button btn-secondary  text-white">Lets Go</label>
+          <input type="checkbox" id="my-modal-4" class="modal-toggle" />
+          <label for="my-modal-4" class="modal cursor-pointer">
+            <label class="modal-box relative shadow-none" for="">
+              <div class="card w-full bg-base-100 ">
+                <figure><img src=${src} alt="Shoes" /></figure>
+                <div class="card-body">
+                  <h2 class="card-title">
+                    ${type}
+                    <div class="badge badge-secondary">NEW</div>
+                  </h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div class="card-actions justify-end text-secondary">
+                    <div class="badge badge-outline hover:text-white hover:bg-secondary ">Capacity: ${capacity}</div> 
+                    <div class="badge badge-outline hover:text-white hover:bg-secondary">Fare/km: $${fpk}</div>
+                  </div>
+                  <div class= 'flex gap-2 mx-auto'> 
+                  <input placeholder='Distance' class='distance h-12 placeholder:text-gray-700 tracking-widest font-mono rounded-lg pl-4 outline-none' type='number'/> <input placeholder='Vehicles Numbers' class='vehicles h-12 placeholder:text-gray-700 tracking-widest font-mono rounded-lg pl-4 outline-none' type='number'/>
+                  </div>
+                  <button class= 'btn btn-secondary text-white'>Submit</button>
+                  
+                </div>
+              </div>
+            </label>
+          </label>
           </div>
         </div>
   `;
@@ -24,4 +48,14 @@ const createElement = (type, src, fpk, capacity) => {
 };
 data.map((e) => {
   createElement(e.type, e.src, e.fpk, e.capacity);
+});
+
+/* --------------------------------------------------------------------- */
+
+main.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    e.target.addEventListener("click", (e) => {
+      console.log("dkfj");
+    });
+  }
 });
